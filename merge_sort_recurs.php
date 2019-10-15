63c5fb04d1c549e6414aff08f67d4bcdd361eae9
Cyril Matté <47034111+freeonisland@users.noreply.github.com>
2019-10-15 20:56:18 +0200

    Update merge_sort_recurs.php

--- merge_sort_recurs.php
+++ merge_sort_recurs.php
@@ -12,16 +12,27 @@ function merge_sort(array $array)
     if(1===count($array)) return $array;
     
     $n_split = floor(count($array) / 2);
-    
     $left = merge_sort(array_slice($array, 0, $n_split));
     $right = merge_sort(array_slice($array, $n_split));
-    //print_r(merge($left, $right));
+
     $r = merge($left, $right);
-    //print_r($r);
     return $r;
 }
 
 function merge(array $left, array $right)
 {
-    return array_merge($left,$right);
+    $res=[];
+    while( count($left)>0 || count($right)>0 ) {
+        if(empty($right)) {
+              $res[] = array_shift($left);
+        } elseif(empty($left)) {
+              $res[] = array_shift($right);
+        } else if(array_slice($left,0,1)<array_slice($right,0,1)) {
+            $res[] = array_shift($left);
+        } else {
+            $res[] = array_shift($right);
+        }
+    }
+    
+    return $res;
 }
