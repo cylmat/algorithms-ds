97ea6749dbb47e7ed5a61dbd095d121690b9f060
Cyril Matté <47034111+freeonisland@users.noreply.github.com>
2020-06-06 21:57:40 +0200

    Update insertion_sort.php

--- src/Algorithms/Sorting/insertion_sort.php
+++ src/Algorithms/Sorting/insertion_sort.php
@@ -30,40 +30,5 @@ $a = [56,12,42,8,57,93,2,16,17,49,85];
 $r = triInsertionIter($a);
 
 
-/**
- * Insertion Sort
- * 
- *  I: geeksforgeeks
- *  O: eeeefggkkorss
- * 
- * - ref: https://www.geeksforgeeks.org/sorting-algorithms
- */
-// for one char
-// switch n-1 <> n
-// until 0
-function string_sort(string $str, int $i): string
-{
-    if (0==$i) {
-        return $str;
-    }
-    
-    //switch
-    if ($str{$i-1} > $str{$i}) {
-        if (!isset($str{$i+1})) {
-            $str = substr($str,0,$i-1) . $str{$i} . $str{$i-1};
-        } else {
-            $str = substr($str,0,$i-1) . $str{$i} . $str{$i-1} . substr($str, $i+1);
-        }
-    }
-    
-    // 
-    return string_sort($str, $i-1);
-}
-
-$str_sort = 'geeksforgeeks';
-for ($i=0; $i<strlen($str_sort); $i++) {
-    $str_sort = string_sort($str_sort, strlen($str_sort)-1);
-}
-
 //res
-echo (int)assert(true && $str_sort == 'eeeefggkkorss');
+echo (int)assert(true);
