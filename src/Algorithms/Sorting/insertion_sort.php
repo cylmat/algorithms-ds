c7c7d81eed4061105d3e9826aed87b82fe190727
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-07-05 16:27:13 +0200

    Update insertion_sort.php

--- src/Algorithms/Sorting/insertion_sort.php
+++ src/Algorithms/Sorting/insertion_sort.php
@@ -30,3 +30,49 @@ $r = triInsertionIter($a);
 
 //res
 echos(true);
+
+
+
+
+
+
+
+
+
+
+/************
+ Other
+*/
+function insertionRec(int[] t, int n, int e) {
+  // n nombre d'éléments de t
+  // e élément à insérer
+  if ((n == 0) || (e >= t[n - 1]))
+      t[n] = e;
+  else {
+      t[n] = t[n - 1];
+      insertionRec(t, n - 1, e);
+  }
+}
+
+function insertionI ( int t[], int n, int e){
+  // n nombre d'éléments de t
+  // e élément à insérer
+  int i;
+  for(i=n; ((i!=0)&&(e<t[i-1])); --i) // arrêt : ((i==0)||(e>=t[i-1]))
+      t[i]=t[i-1];
+  t[i]=e;
+}
+
+
+/////////////////////////////////////:
+ triInsertionR(int t[], int n) {
+  if (n > 1) {
+      triInsertionR(t, n - 1);
+      insertionRec(t, n - 1, t[n - 1]);
+  }
+}
+
+ triInsertionI(int t[], int n){
+  for(int i=1; i<n; ++i)
+      triInsertionI(t, i, t[i]);
+}
