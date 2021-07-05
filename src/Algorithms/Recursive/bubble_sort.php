7b7752aa7a8d51b22ca6fa69d02d283ef3aece83
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-07-05 16:23:13 +0200

    Create bubble_sort.php

---
+++ src/Algorithms/Recursive/bubble_sort.php
@@ -0,0 +1,22 @@
+<?php
+
+function bubble_rec($n, &$a, &$swap, $i, $j): bool //return if a swap append or not
+{
+    if ($i>$n-2 || $j>$n-1) return; // $j from $i to last
+   
+    $current_action=false;
+    if ($a[$j-1] > $a[$j]) { //asc
+        [$a[$j-1], $a[$j]] = [$a[$j], $a[$j-1]]; //swap
+        $swap++;
+        $current_action=true;
+    }
+
+    $child_action = bubble_rec($n, $a, $swap, $i, $j+1); // check every $j
+   
+    if ($current_action===false && $child_action===false) {
+        //optimisation: if no swap this time, array is sorted
+        return false;
+    }
+    bubble_rec($n, $a, $swap, $i+1, $i+1); // then next $i, reset $j
+    return true;
+}
