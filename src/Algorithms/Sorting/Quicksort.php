d46b58f2cd4b03098afd07d804fb98bc796a3a6d
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-08-30 20:12:36 +0200

    Create Quicksort.php

---
+++ src/Algorithms/Sorting/Quicksort.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @ref: https://www.interviewbit.com/tutorial/quicksort-algorithm/
+ */
+
+function quickSort(&$array, int $low, int $high): void
+{
+    if ($low < $high)
+    {
+        // pivot_index is partitioning index, arr[pivot_index] is now at correct place in sorted array
+        $pivot_index = qs_partition($array, $low, $high);
+
+        quickSort($array, $low, $pivot_index - 1);  // Before pivot_index
+        quickSort($array, $pivot_index + 1, $high); // After pivot_index
+    }
+}
+
+function qs_partition (array &$array, int $low, int $high): int
+{
+    // pivot - Element at right most position
+    $pivot = $array[$high];  
+    $i = ($low - 1);  // Index of smaller element
+    for ($j = $low; $j <= $high-1; $j++)
+    {
+        // If current element is smaller than the pivot, swap the element with pivot
+        if ($array[$j] < $pivot)
+        {
+            $i++;    // increment index of smaller element
+            [$array[$i], $array[$j]] = [$array[$j], $array[$i]];
+        }
+    }
+    [$array[$i + 1], $array[$high]] = [$array[$high], $array[$i + 1]];
+    return ($i + 1);
+}
+
+$array = [50, 23, 9, 18, 61, 32];
+quickSort($array, 0, count($array)-1);
+
+validates($array, [9, 18, 23, 32, 50, 61]);
