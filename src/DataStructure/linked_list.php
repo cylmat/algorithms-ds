c5a39650375872acd62a0ebb5e895e1100ce9f47
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-20 17:06:27 +0100

    Update linked_list.php

--- src/DataStructure/linked_list.php
+++ src/DataStructure/linked_list.php
@@ -1,24 +1,48 @@
 <?php
 
-function linked_list(?Node $head, $data) { //Node(->data)(->next)
-    if(null === $head) {
+class Node
+{
+    public $value, $next;
+    function __construct(?string $value=null) { $this->value = $value; }
+}
+
+function createNodeList(Node $head, string $data)
+{
+    if (null === $head) {
+        $head = new Node($data);
+        return $head;
+    }
+
+    // iteratif way
+    $child = $head; 
+    $newNode = new Node($data);
+    while ($child->next !== null) 
+        $child = $child->next; 
+    $child->next = $newNode;
+
+    return $newNode;
+}
+
+function createLinkedList_rec(Node $head, string $data)
+{
+    if (null === $head) {
         $head = new Node($data);
         return $head;
     }
     // recursif way
-    $head->next = $this->linked_list($head->next, $data);
+    $head->next = createLinkedList_rec($head->next, $data);
 
     // iteratif way
     $child = $head; 
-    while($child->next !== null) 
+    while ($child->next !== null) 
         $child = $child->next; 
     $child->next = new Node($data);
 
     return $head;
 }
 
-function removeDuplicates($head) {
-      $point=$head;
+function removeDuplicates(LNode $head) {
+      $point = $head;
       while (null !== $point->next) {
           if ($point->data == $point->next->data) {
               $point->next = $point->next->next;
@@ -40,3 +64,24 @@ function removeDuplicates_rec($head) {
       $this->removeDuplicates($head->next);
       return $head;
 }
+
+$node1 = createNodeList(new Node('head'), 'my-val1');
+$node2 = createNodeList($node1, 'my-val2');
+
+validates('my-val2', $node2->value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
