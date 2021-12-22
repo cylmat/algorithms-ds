67e2d9588860fb327869670befac5e64cd9486fd
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2021-12-22 22:15:59 +0100

    Update and rename src/Algorithms/Recursive/reverse.php to src/Recursive/reverse_string.php

--- src/Recursive/reverse_string.php
+++ src/Recursive/reverse_string.php
@@ -6,16 +6,15 @@
  * 
  * ref: https://www.geeksforgeeks.org/reverse-a-string-using-recursion/
  */
-function reverse(string $arr, string $out=''): string
+function reverse_string_rec(string $arr, string $out=''): string
 { 
     if (strlen($arr)==0) return $out;
     
     $out .= substr($arr,-1,1);
-    return reverse(substr($arr,0,strlen($arr)-1), $out);
+    return reverse_string_rec(substr($arr,0,strlen($arr)-1), $out);
 } 
 
 $str = "alphabeta"; 
-$res = reverse($str);
-
+$res = reverse_string_rec($str);
 
 validates ($res, 'atebahpla'); 
