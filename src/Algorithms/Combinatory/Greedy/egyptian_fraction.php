1640d7c88bb2cfb6a22bbb200cf2242566322a8b
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-01-04 20:28:03 +0100

    Update egyptian_fraction.php

--- src/Algorithms/Combinatory/Greedy/egyptian_fraction.php
+++ src/Algorithms/Combinatory/Greedy/egyptian_fraction.php
@@ -9,30 +9,35 @@
 * 1) For 6/14, find ceiling of 14/6 which is 3
 * 2) Continue with 6/14 - 1/3
 *
-* ref: https://www.geeksforgeeks.org/greedy-algorithm-egyptian-fraction/
+* @see https://www.geeksforgeeks.org/greedy-algorithm-egyptian-fraction/
 */
 function egyptian_fraction(int $nr, int $dr, $result=[]): array
 {
     // if 0
     if ($nr==0 || $dr==0) return $result;
+
     // fraction of 1/n
     if ($dr % $nr == 0) {
         $result[] = (int)$dr/$nr;
         return $result;
     }
+
     // integer
     if ($nr % $dr == 0) {
         $result[] = (int)$nr/$dr;
         return $result;
     }
+
     // others cases
     if ($nr > $dr) {
         $result[] = (int)($nr/$dr);
+ 
         return egyptian_fraction($nr % $dr, $dr, $result);
     }
     // dr/nr && $dr%$nr
     $n = (int)($dr / $nr) + 1;
     $result[] = $n;
+ 
     return egyptian_fraction($nr * $n - $dr, $dr * $n, $result);
 }
 
