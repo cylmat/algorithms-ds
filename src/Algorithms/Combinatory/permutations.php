1d52b79d67da30b98162072274304bb0ca880462
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2022-01-04 21:12:01 +0100

    Update permutations.php

--- src/Algorithms/Combinatory/permutations.php
+++ src/Algorithms/Combinatory/permutations.php
@@ -1,18 +1,17 @@
 <?php
+
 /**
  * Permutation
  * - Print all sentences possible taking one word from a list
  * 
- * refs: 
- *  https://www.topcoder.com/generating-permutations
- *  https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python
+ * @see https://www.geeksforgeeks.org/generate-all-the-permutation-of-a-list-in-python
  */
 
 /*
  * Input: [a,b,c]
  * Output: abc, bac, acb, cab, bca, cba
  * 
- * ref: https://docstore.mik.ua/orelly/webprog/pcook/ch04_26.htm#phpckbk-CHP-4-EX-6
+ * @see  https://docstore.mik.ua/orelly/webprog/pcook/ch04_26.htm#phpckbk-CHP-4-EX-6
  */
 function print_words_permutations(array $array, array $perms=[]): string
 { 
@@ -34,21 +33,22 @@ function print_words_permutations(array $array, array $perms=[]): string
     return $out;
 } 
 $res = print_words_permutations(['a','b']);
-
 validates($res, "a b\nb a\n");
 
 /**
  * Print all combinations of -lists- of words
  * 
- * https://www.techiedelight.com/combinations-phrases-formed-picking-words-lists/
- * 
+ * @see https://www.techiedelight.com/combinations-phrases-formed-picking-words-lists/
  */
+$all_combinations = '';
 function printAllCombinations(array $list, int $n, string $res, int $list_num): void
 {
+	global $all_combinations;
+
 	// if we have traversed each list
 	if ($list_num == $n) {
 		// print phrase after removing trailing space
-		echo substr($res,1) . "\n";
+		$all_combinations .= substr($res,1) . "\n";
 		return;
 	}
 
@@ -56,8 +56,7 @@ function printAllCombinations(array $list, int $n, string $res, int $list_num): 
 	$m = count($list[$list_num]);
 
 	// do for each word in current list
-	for ($i = 0; $i < $m; $i++)
-	{
+	for ($i = 0; $i < $m; $i++) {
 		// append current word to output
 		$out = $res . " " . $list[$list_num][$i];
 
@@ -65,54 +64,53 @@ function printAllCombinations(array $list, int $n, string $res, int $list_num): 
 		printAllCombinations($list, $n, $out, $list_num + 1);
 	}
 }
-$list = [
-	[ "alpha", "beta" ], [ "zoo", "yield" ], [ "000", "111" ]
-];
+$list = [ ["alpha","beta"], ["zoo","yield"], ["000","111"] ];
 
-ob_start();
 printAllCombinations($list, count($list), "", 0);
-$res = trim(ob_get());
 
-validates($res, "alpha zoo 000
+validates($all_combinations, 
+"alpha zoo 000
 alpha zoo 111
 alpha yield 000
 alpha yield 111
 beta zoo 000
 beta zoo 111
 beta yield 000
-beta yield 111");
+beta yield 111
+");
 
 /** 
  * Backtracking approach
  * 
- * ref: https://stackoverflow.com/questions/2617055/how-to-generate-all-permutations-of-a-string-in-php
+ * @see https://stackoverflow.com/questions/2617055/how-to-generate-all-permutations-of-a-string-in-php
  */
+$backtrack_permut = '';
 function backtrack_string_permute(string $str, $i, $n): void
 {
-   if ($i == $n) {
-       echo "$str\n";
-   } else {
+    global $backtrack_permut;
+    
+    if ($i == $n) {
+        $backtrack_permut .= "$str\n";
+    } else {
         for ($j = $i; $j < $n; $j++) {
-          //swap ($i) and ($j);
-          [$str[$i], $str[$j]] = [$str[$j], $str[$i]];
+            //swap ($i) and ($j);
+            [$str[$i], $str[$j]] = [$str[$j], $str[$i]];
 
-          backtrack_string_permute($str, $i+1, $n);
-          [$str[$i], $str[$j]] = [$str[$j], $str[$i]]; // backtrack.
+            backtrack_string_permute($str, $i+1, $n);
+            [$str[$i], $str[$j]] = [$str[$j], $str[$i]]; // backtrack.
        }
    }
 }
+backtrack_string_permute("ABC", 0, strlen("ABC")); // call the function.
 
-$str = "ABC";
-ob_start();
-backtrack_string_permute($str, 0, strlen($str)); // call the function.
-$res = ob_get();
-
-validates($res, "ABC
+validates($backtrack_permut, 
+"ABC
 ACB
 BAC
 BCA
 CBA
-CAB");
+CAB
+");
 
 /*
  * Permutation of array
@@ -121,7 +119,7 @@ CAB");
  */
 function permute_array(array $array): array 
 {
-    if(1 === count($array)) {
+    if (1 === count($array)) {
         return $array;
     }
         
@@ -134,40 +132,43 @@ function permute_array(array $array): array
     return $result;
 }
 $res_a = permute_array(['a','b','c']);
-
 validates($res_a, ['abc','acb','bac','bca','cab','cba']);
+
 
 /*
 * Print all rotations of input string recursively
 * - abc -> abc, acb, cab, cba, bca, bac (in the opposite way)
 */
+$permut_chars = '';
 function permute_chars(string $str, int $index=0, int $count=0): void
 {
-    if($count == strlen($str)-$index) {
+    global $permut_chars;
+
+    if ($count == strlen($str)-$index) {
         return;
     }
 
     //transfert $index at the end of string 
     //al(p)ha -> alha(p)
     $str = substr($str, 0, $index) . substr($str, $index+1) . $str[$index];
-    if ($index==strlen($str)-2) { //reached to the end, print it
-        echo "$str\n";//or keep it in an array
+    if ($index == strlen($str)-2) { //reached to the end, print it
+        $permut_chars .= "$str\n";//or keep it in an array
     }
 
     permute_chars($str, $index+1); //rotate its children
     permute_chars($str, $index, $count+1); //rotate itself
 }
 
-ob_start();
 permute_chars('abc'); //rotate its children
-$res = ob_get();
-
-validates($res, "bac
+validates($permut_chars, 
+"bac
 bca
 cba
 cab
 acb
-abc");
+abc
+");
+
 
 /**
  * Permutation iterative way
@@ -176,22 +177,23 @@ abc");
  * Input : [1,2,3]
  * Output: 123, 132, 213, 231, 312, 321
  * 
- * refs: 
+ * @see
  *  http://villemin.gerard.free.fr/aInforma/11Recurs.htm#pemute
  *  https://www.tutorialspoint.com/all-permutations-of-a-string-using-iteration
  */
-function permut_iter(array $arr): void  
+function permut_iter(array $arr): string  
 {
-   sort($arr); //sort by value
+    $res = '';
+    sort($arr); //sort by value
 
-   while (true) {
-      echo implode(' ',$arr).PHP_EOL;
+    while (true) {
+      $res .= implode(' ',$arr).PHP_EOL;
 
       $i = sizeof($arr)-1;
       //end when all keys are inverteds
       while ($arr[$i-1] >= $arr[$i]) {
          if (--$i == 0)
-            return;
+            return $res;
       }
 
       $j = sizeof($arr)-1;
@@ -204,15 +206,16 @@ function permut_iter(array $arr): void
       [$arr[$i-1], $arr[$j]] = [$arr[$j], $arr[$i-1]];
       $arr = array_merge(array_slice($arr, 0, $i), array_reverse(array_slice($arr, $i)));
    }
+   
+   return $res;
 }
 
-ob_start();
-permut_iter([2,1,3]);
-$res = ob_get();
-
-validates($res, "1 2 3
+$res = permut_iter([2,1,3]);
+validates($res, 
+"1 2 3
 1 3 2
 2 1 3
 2 3 1
 3 1 2
-3 2 1");
+3 2 1
+");
