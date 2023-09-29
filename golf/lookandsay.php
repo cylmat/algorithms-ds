356dc03c495545dc51cbb3d9c428eb32f11f3a6f
Cyril Matté <47034111+cylmat@users.noreply.github.com>
2023-09-29 09:44:35 +0200

    Create lookandsay.php

---
+++ golf/lookandsay.php
@@ -0,0 +1,15 @@
+<?php 
+
+$s=1;
+foreach(range(0,6)as$r){
+  echo$t="$s\n";
+  $s='';
+  $l=$t[0];
+  $n=1;
+  for($i=1;$i<strlen($t);$i++) {
+    $h=($c=$t[$i])!==$l;
+    $h?$s.=$n.$l:0;
+    $n=$h?1:$n+1;
+    $l=$c;
+  }
+}
